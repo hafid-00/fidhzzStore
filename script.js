e0b89a799a3e919abc5d10a00c51a5ad11227e05
hafid-00 <coc29913@gmail.com>
2025-10-07 19:13:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton')
-tombolSapa.addEventListener('click', funcation() {
+tombolSapa.addEventListener('click', function() {
   alert('Halo!Terima kasih sudah berkunjung!');
 });
